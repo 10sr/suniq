fac322ac9d156e098a402517f1f91b09b363213a
10sr <8slashes+git@gmail.com>
2013-06-04 17:30:58 +0900

    suniq basically works

--- suniq.c
+++ suniq.c
@@ -1,25 +1,59 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #include"counter.h"
 
-char *ReadOneLine(int fd_input, char delim)
+const int BUF_LEN = 10000;
+
+int ReadOneLine(FILE *fp_input, char delim, char *out, int len)
 {
     /* Read one segment from fd_input. Input is separated by delim char.
-     * At the end of input NULL is returned.
+     * At the end of input returns 1, otherwise return 0.
      */
-    return NULL;
+    int i;
+    int c;
+
+    for (i = 0; i < len; i++) {
+        c = fgetc(fp_input);
+        if ((int)delim == c) {
+            out[i] = '\0';
+            return 0;
+        } else if (c == EOF) {
+            out[i] = '\0';
+            return 1;
+        }
+        out[i] = (char)c;
+    }
+    fprintf(stderr, "%s: Too long line", "suniq");
+    exit(1);
+    return 1;
 }
 
-int suniq(int fd_input, char delim)
+int suniq(FILE *fp_input, char delim, int print_num)
 {
-    char *line;
-    line = ReadOneLine(fd_input, delim);
-    
-    while (line) {
-        
+    char line[BUF_LEN];
+    int r;
+    struct Counter *c;
+
+    c = Counter_Create();
+    if (! c) {
+        fprintf(stderr, "%s: Cannot allocate memory", "suniq");
+        exit(1);
+    }
+
+    r = ReadOneLine(fp_input, delim, line, BUF_LEN);
+
+    while (r == 0) {
+        Counter_AddStr(c, line);
+        /* printf("%s\n", line); */
+        r = ReadOneLine(fp_input, delim, line, BUF_LEN);
+    }
+
+    Counter_Print(c, stdout, print_num);
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    return suniq(stdin, '\n');
+    return suniq(stdin, '\n', 1);
 }
